d9310681b39195759b2d0d4f47cdf40ec3421c56
Swifter <swifter1243@gmail.com>
2025-02-21 14:19:11 -0500

    init

---
+++ script.ts
@@ -0,0 +1,34 @@
+import * as rm from "https://deno.land/x/remapper@4.0.0/src/mod.ts"
+import * as bundleInfo from './bundleinfo.json' with { type: 'json' }
+
+const pipeline = await rm.createPipeline({ bundleInfo })
+
+const bundle = rm.loadBundle(bundleInfo)
+const materials = bundle.materials
+const prefabs = bundle.prefabs
+
+// ----------- { SCRIPT } -----------
+
+async function doMap(file: rm.DIFFICULTY_NAME) {
+    const map = await rm.readDifficultyV3(pipeline, file)
+
+    // Example: Run code on every note!
+
+    // map.allNotes.forEach(note => {
+    //     console.log(note.beat)
+    // })
+
+    // For more help, read: https://github.com/Swifter1243/ReMapper/wiki
+}
+
+await Promise.all([
+    doMap('ExpertPlusStandard'),
+    doMap('ExpertStandard'),
+    doMap('HardStandard')
+])
+
+// ----------- { OUTPUT } -----------
+
+pipeline.export({
+    outputDirectory: '../OutputMaps/Aether'
+})
