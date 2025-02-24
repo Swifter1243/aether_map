3596bb7614f5ed96114b8973adfa3b3a1c2b614a
Frautcres <Frautcres@FRAUTCRES>
2025-02-24 03:40:32 -0500

    SimplePostProcessing update.

--- Aether_unity_2019/Assets/VivifyTemplate/Utilities/Scripts/SimplePostProcessing.cs
+++ Aether_unity_2019/Assets/VivifyTemplate/Utilities/Scripts/SimplePostProcessing.cs
@@ -1,4 +1,10 @@
 ﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine.Rendering;
+#endif
+
+
 namespace VivifyTemplate.Utilities.Scripts
 {
     [ExecuteInEditMode]
@@ -7,6 +13,9 @@ namespace VivifyTemplate.Utilities.Scripts
     {
         public Material postProcessingMaterial;
         public int pass;
+        public bool sceneViewEnabled;
+
+        private string commandBufferID;
 
         private void OnRenderImage(RenderTexture src, RenderTexture dst) {
             if(postProcessingMaterial != null) {
@@ -18,10 +27,69 @@ namespace VivifyTemplate.Utilities.Scripts
         }
 
         private void OnEnable() {
-            var thisCamera = GetComponent<Camera>();
+            commandBufferID = name + GetHashCode().ToString();
+
+
+            Camera thisCamera = GetComponent<Camera>();
             if (thisCamera != null) {
                 thisCamera.depthTextureMode |= DepthTextureMode.Depth | DepthTextureMode.MotionVectors | DepthTextureMode.DepthNormals;
             }
+#if UNITY_EDITOR
+            Camera.onPreRender += OnPreRenderCallback;
+#endif
         }
+
+
+#if UNITY_EDITOR
+		private void OnDisable()
+		{
+            Camera.onPreRender -= OnPreRenderCallback;
+        }
+
+        //Camera.AddCommandBuffer
+        private void OnPreRenderCallback(Camera camera)
+        {
+            SceneView sceneView = SceneView.lastActiveSceneView;
+            if (sceneView != null && sceneView.camera == camera)
+			{
+                CommandBuffer command = GetBlitCommand(camera);
+                if (sceneViewEnabled)
+				{
+                    if (command == null)
+					{
+                        RenderTargetIdentifier src = new RenderTargetIdentifier(BuiltinRenderTextureType.CurrentActive);
+                        RenderTargetIdentifier dst = new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget);
+                        command = new CommandBuffer();
+                        command.name = commandBufferID;
+
+                        if (postProcessingMaterial != null)
+                        {
+                            command.Blit(src, dst, postProcessingMaterial,
+                                (pass >= 0) ? pass : -1);
+                        }
+                        else
+                        {
+                            command.Blit(sceneView.camera.activeTexture, sceneView.camera.targetTexture);
+                        }
+                        sceneView.camera.AddCommandBuffer(CameraEvent.AfterImageEffects, command);
+                    }
+                }
+				else
+                {
+                    camera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, command);
+                }
+            }
+        }
+
+        private CommandBuffer GetBlitCommand(Camera camera)
+		{
+            CommandBuffer[] commands = camera.GetCommandBuffers(CameraEvent.AfterImageEffects);
+            for(int i = 0; i < commands.Length; i++)
+			{
+                if (commands[i].name == commandBufferID) return commands[i];
+			}
+            return null;
+		}
+#endif
     }
 }
