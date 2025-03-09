440d004f551350e360ad9bbaa4e5522992c39072
Frautcres <Frautcres@FRAUTCRES>
2025-03-09 13:39:23 -0400

    Pushing this here too

--- Aether_unity_2019/Assets/VivifyTemplate/Utilities/Scripts/SimplePostProcessing.cs
+++ Aether_unity_2019/Assets/VivifyTemplate/Utilities/Scripts/SimplePostProcessing.cs
@@ -13,11 +13,13 @@ namespace VivifyTemplate.Utilities.Scripts
     {
         public Material postProcessingMaterial;
         public int pass;
-        public bool sceneViewEnabled;
 
+        public bool sceneViewEnabled = false;
+        private Camera sceneViewCamera = null;
         private string commandBufferID;
 
         private void OnRenderImage(RenderTexture src, RenderTexture dst) {
+            //TODO: use command buffers.
             if(postProcessingMaterial != null) {
                 Graphics.Blit(src, dst, postProcessingMaterial,
                     (pass >= 0) ? pass : -1);
@@ -41,47 +43,69 @@ namespace VivifyTemplate.Utilities.Scripts
 
 
 #if UNITY_EDITOR
-		private void OnDisable()
-		{
+        private void OnDisable()
+        {
+            //sceneViewEnabled = false;
+
             Camera.onPreRender -= OnPreRenderCallback;
+            if (sceneViewCamera != null)
+			{
+                CommandBuffer command = GetBlitCommand(sceneViewCamera);
+                if (command != null) sceneViewCamera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, command);
+            }
+            sceneViewCamera = null;
         }
 
         private void OnPreRenderCallback(Camera camera)
         {
-            SceneView sceneView = SceneView.lastActiveSceneView;
+            SceneView sceneView = SceneView.currentDrawingSceneView;
             if (sceneView != null && sceneView.camera == camera)
 			{
                 CommandBuffer command = GetBlitCommand(camera);
-                bool isCommand = command == null;
+                bool isCommandNotFound = (command == null);
+
+                //Somehow the scene camera is not the same.
+                if (sceneViewCamera != camera)
+				{
+                    if (sceneViewCamera != null && !isCommandNotFound) camera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, command);
+                    sceneViewCamera = camera;
+                    isCommandNotFound = true;
+				}
+
                 if (sceneViewEnabled)
 				{
-                    if (isCommand)
-                    {
-                        RenderTargetIdentifier src = new RenderTargetIdentifier(BuiltinRenderTextureType.CurrentActive);
-                        RenderTargetIdentifier dst = new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget);
-                        command = new CommandBuffer();
-                        command.name = commandBufferID;
-
-                        if (postProcessingMaterial != null)
-                        {
-                            command.Blit(src, dst, postProcessingMaterial,
-                                (pass >= 0) ? pass : -1);
-                        }
-                        else
-                        {
-                            command.Blit(sceneView.camera.activeTexture, sceneView.camera.targetTexture);
-                        }
-                        sceneView.camera.AddCommandBuffer(CameraEvent.AfterImageEffects, command);
-                    }
-                }
-				else if (!isCommand)
+                    //Enabled and command does not exist
+                    if (isCommandNotFound) AddCommand(sceneView.camera);
+				}
+				else if (!isCommandNotFound) 
                 {
+                    //Command exists, remove it.
                     camera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, command);
                 }
             }
         }
 
-        private CommandBuffer GetBlitCommand(Camera camera)
+		private void AddCommand(Camera camera)
+		{
+            CommandBuffer command;
+			RenderTargetIdentifier src = new RenderTargetIdentifier(BuiltinRenderTextureType.CurrentActive);
+			RenderTargetIdentifier dst = new RenderTargetIdentifier(BuiltinRenderTextureType.CameraTarget);
+			command = new CommandBuffer();
+			command.name = commandBufferID;
+
+			if (postProcessingMaterial != null)
+			{
+				command.Blit(src, dst, postProcessingMaterial,
+					(pass >= 0) ? pass : -1);
+			}
+			else
+			{
+				command.Blit(camera.activeTexture, camera.targetTexture);
+			}
+			camera.AddCommandBuffer(CameraEvent.AfterImageEffects, command);
+		}
+
+		private CommandBuffer GetBlitCommand(Camera camera)
 		{
             CommandBuffer[] commands = camera.GetCommandBuffers(CameraEvent.AfterImageEffects);
             for(int i = 0; i < commands.Length; i++)
