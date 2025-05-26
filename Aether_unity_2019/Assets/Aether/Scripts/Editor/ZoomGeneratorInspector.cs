5336cc1cd9879948dda92b89bdea4f6e7169b7f9
Swifter <swifter1243@gmail.com>
2025-05-26 18:17:15 -0400

    begin zoom generator

---
+++ Aether_unity_2019/Assets/Aether/Scripts/Editor/ZoomGeneratorInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+namespace Aether.Scripts.Editor
+{
+	[CustomEditor(typeof(ZoomGenerator))]
+	public class ZoomGeneratorInspector : UnityEditor.Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			ZoomGenerator zoomGenerator = (ZoomGenerator)target;
+
+			GUILayout.Space(10);
+			if (GUILayout.Button("Generate", GUILayout.Height(30)))
+			{
+				zoomGenerator.GenerateZooms();
+			}
+
+			GUILayout.Space(10);
+			if (GUILayout.Button("Clear Previous", GUILayout.Height(30)))
+			{
+				zoomGenerator.ClearPrevious();
+			}
+		}
+	}
+}
---
+++ Aether_unity_2019/Assets/Aether/Scripts/ZoomGenerator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+namespace Aether.Scripts
+{
+    public class ZoomGenerator : MonoBehaviour
+    {
+        [Serializable]
+        public struct ZoomObject
+        {
+            [SerializeField] public GameObject prefab;
+        }
+
+        public ZoomObject[] objects;
+        public GameObject parent;
+        public AnimationClip clip;
+        public float startTime;
+        public float endTime;
+        public float zoomDuration = 10;
+        public float zoomFrequency = 3;
+        public Vector3 zoomEndSize = Vector3.one * 1000;
+
+
+        private void DestroyChildren(Transform transform)
+        {
+            while (transform.childCount > 0)
+            {
+                // god, I hate children.
+                DestroyImmediate(transform.GetChild(0).gameObject);
+            }
+        }
+
+        public void ClearPrevious()
+        {
+            if (parent == null || clip == null) return;
+
+            Transform parentObj = parent.transform;
+            Transform rootObj = parentObj.root;
+
+            // 1. Clear children
+            DestroyChildren(parentObj);
+
+            // 2. Clear animation on parent and its children
+            ClearAnimations(rootObj, parentObj, clip);
+        }
+
+        public void GenerateZooms()
+        {
+            if (parent == null || clip == null) return;
+
+            ClearPrevious();
+
+            Transform parentObj = parent.transform;
+            Transform rootObj = parentObj.root;
+
+            // 3. Instantiate and animate
+            float currentStartTime = startTime;
+
+            while (currentStartTime < endTime)
+            {
+                ZoomObject zoom = objects[(int)(Random.value * objects.Length)];
+                if (zoom.prefab == null) continue;
+
+                GameObject instance = Instantiate(zoom.prefab, parentObj);
+                instance.transform.localScale = Vector3.zero;
+                instance.transform.localPosition = Vector3.zero;
+                instance.transform.localRotation = Quaternion.identity;
+                instance.name = $"Zoom_{currentStartTime}";
+
+                string path = AnimationUtility.CalculateTransformPath(instance.transform, rootObj);
+
+                AddZoomInAnimation(clip, path, currentStartTime, currentStartTime + zoomDuration);
+
+                currentStartTime += zoomFrequency;
+            }
+
+            AssetDatabase.SaveAssets();
+        }
+
+        private void ClearAnimations(Transform root, Transform parent, AnimationClip clip)
+        {
+            string parentPath = AnimationUtility.CalculateTransformPath(parent, root);
+
+            foreach (var binding in AnimationUtility.GetCurveBindings(clip))
+            {
+                bool isAtLeastParent = binding.path.StartsWith(parentPath);
+                if (isAtLeastParent)
+                {
+                    AnimationUtility.SetEditorCurve(clip, binding, null);
+                }
+            }
+        }
+
+        private void AddZoomInAnimation(AnimationClip clip, string path, float startTime, float endTime)
+        {
+            AnimationCurve curveX = AnimationCurve.Linear(startTime, 0, endTime, zoomEndSize.x);
+            AnimationCurve curveY = AnimationCurve.Linear(startTime, 0, endTime, zoomEndSize.y);
+            AnimationCurve curveZ = AnimationCurve.Linear(startTime, 0, endTime, zoomEndSize.z);
+
+            clip.SetCurve(path, typeof(Transform), "m_LocalScale.x", curveX);
+            clip.SetCurve(path, typeof(Transform), "m_LocalScale.y", curveY);
+            clip.SetCurve(path, typeof(Transform), "m_LocalScale.z", curveZ);
+        }
+    }
+}
