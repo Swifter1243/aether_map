00c56b100a22f7652fcebb009e587a8afb367120
Swifter <swifter1243@gmail.com>
2025-05-26 23:28:47 -0400

    this is useless

--- Aether_unity_2019/Assets/Aether/Scripts/PrefabSaver.cs
+++ Aether_unity_2019/Assets/Aether/Scripts/PrefabSaver.cs
@@ -5,10 +5,5 @@ namespace Aether.Scripts
 	public class PrefabSaver : MonoBehaviour
 	{
 		public GameObject destination;
-
-		private void OnSave()
-		{
-			// TODO: Save this object to destination prefab.
-		}
 	}
 }
