20cc68ba98f9513c90cc087104df51f297c28391
Swifter <swifter1243@gmail.com>
2025-06-21 17:01:17 -0400

    use intro animation instead of spawning ingame

--- script.ts
+++ script.ts
@@ -8,6 +8,7 @@ const materials = bundle.materials
 const prefabs = bundle.prefabs
 
 const TIMES = {
+    INTRO: 0,
     INTRO1: 5,
     INTRO2: 37,
     INTRO3: 64,
@@ -87,26 +88,6 @@ function visualsSetup(map: rm.V3Difficulty) {
     })
 }
 
-function moveScene(map: rm.V3Difficulty, prefab: rm.Prefab, start: number, end: number, movementSpeed = 0.5) {
-    const dur = end - start
-
-    const instance = prefab.instantiate(map, start)
-
-    rm.animateTrack(map, {
-        beat: start,
-        track: instance.track.value,
-        duration: dur,
-        animation: {
-            position: [
-                [0, 0, 0, 0],
-                [0, 0, -dur * movementSpeed, 1]
-            ]
-        }
-    })
-
-    instance.destroyObject(end)
-}
-
 function bokeh(material: rm.Material, map: rm.AbstractDifficulty, beat: number, duration = 10, radius = 25)
 {
     material.blit(map, {
@@ -126,40 +107,13 @@ function intro(map: rm.V3Difficulty) {
             skybox: materials.introskybox.path
         }
     })
+
+    const introScene = prefabs.intro.instantiate(map, TIMES.INTRO)
     
-    moveScene(map, prefabs.intro_1, TIMES.INTRO1, TIMES.INTRO2)
     bokeh(materials.introbokeh, map, TIMES.INTRO1, 10, 15)
-    moveScene(map, prefabs.intro_2, TIMES.INTRO2, TIMES.INTRO3)
     bokeh(materials.introbokeh, map, TIMES.INTRO2, 10, 15)
 
-    const tempScene = prefabs.intro_2.instantiate(map, TIMES.INTRO3)
-
-    const vortexTexture1 = rm.createScreenTexture(map, {
-        beat: TIMES.DROP_INTRO,
-        id: '_VortexTexture1'
-    })
-
-    const vortexDuration = TIMES.DROP - TIMES.DROP_INTRO
-
-    rm.blit(map, {
-        beat: TIMES.DROP_INTRO,
-        asset: materials.vortexblit.path,
-        destination: vortexTexture1.id,
-        pass: 0,
-        priority: 0,
-        duration: vortexDuration
-    })
-
-    rm.blit(map, {
-        beat: TIMES.DROP_INTRO,
-        asset: materials.vortexblit.path,
-        pass: 1,
-        priority: 1,
-        duration: vortexDuration
-    })
-
-    vortexTexture1.destroyObject(TIMES.DROP)
-    tempScene.destroyObject(TIMES.DROP)
+    introScene.destroyObject(TIMES.DROP)
 }
 
 function drop(map: rm.V3Difficulty)
