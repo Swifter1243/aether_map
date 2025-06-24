c8d06ddce577d4bd50323d554193ac3deee2db34
Swifter <swifter1243@gmail.com>
2025-06-24 16:16:50 -0400

    animate terrain light ingame

--- src/sections/buildup.ts
+++ src/sections/buildup.ts
@@ -1,13 +1,17 @@
 import { TIMES } from "../constants.ts";
 import { rm } from "../deps.ts";
 import { fadeWhite } from "../effects.ts";
-import { prefabs } from "../main.ts";
+import { materials, prefabs } from "../main.ts";
 
 export function buildup(map: rm.V3Difficulty)
 {
     const buildupScene = prefabs.buildup.instantiate(map, TIMES.BUILDUP)
 
     fadeWhite(map, TIMES.BUILDUP, 16)
+
+    materials["541 - terrain"].set(map, {
+        _Light1Strength: [[0, 0], [1, 1]]
+    }, 541, 20)
 
     buildupScene.destroyObject(TIMES.OUTRO)
 }
