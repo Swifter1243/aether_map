a0b8665f1f3a039ed99685fa5b6b5f652dc2d6e1
Swifter <swifter1243@gmail.com>
2025-07-15 14:07:10 -0400

    add lightshow for timing guides

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,10 @@ export const prefabs = bundle.prefabs
 
 // ----------- { SCRIPT } -----------
 
+// contains timing guides!
+export const lightshow = await rm.readDifficultyV3(pipeline, 'ExpertPlusLightshow')
+delete pipeline.info.difficultyBeatmaps['ExpertPlusLightshow.dat']
+
 async function doMap(file: rm.DIFFICULTY_NAME) {
     const map = await rm.readDifficultyV3(pipeline, file)
 
