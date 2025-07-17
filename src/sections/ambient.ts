6c2c2b763984e37673d45aa4ade09bb14518f603
Swifter <61858676+Swifter1243@users.noreply.github.com>
2025-07-17 15:06:00 -0400

    fake notes during ambient

--- src/sections/ambient.ts
+++ src/sections/ambient.ts
@@ -2,6 +2,7 @@ import { TIMES } from "../constants.ts";
 import { rm } from "../deps.ts";
 import { bokeh } from "../effects.ts";
 import { materials, prefabs } from "../main.ts";
+import { between } from "../utilities.ts"
 
 export function ambient(map: rm.V3Difficulty)
 {
@@ -9,5 +10,32 @@ export function ambient(map: rm.V3Difficulty)
 
     bokeh(materials["261 - bokeh"], map, TIMES.DROP_END, 10, 4)
 
+    doNotemods(map)
+
     ambientScene.destroyObject(TIMES.BRIDGE)
+}
+
+function doNotemods(map: rm.V3Difficulty) {
+    doDisappearing()
+
+    function doDisappearing() {
+        const DISAPPEAR_BEAT_START = 306.01
+        const DISAPPEAR_DURATION = 20
+        const DISAPPEAR_TRACK = "disappeared"
+
+        const disappeared = between(DISAPPEAR_BEAT_START, DISAPPEAR_BEAT_START + DISAPPEAR_DURATION)
+        map.allNotes.filter(disappeared).forEach(x => {
+            x.track.add(DISAPPEAR_TRACK)
+            if (!(x instanceof rm.Arc)) {
+                x.fake = true
+            }
+        })
+        rm.animateTrack(map, {
+            beat: DISAPPEAR_BEAT_START,
+            track: DISAPPEAR_TRACK,
+            animation: {
+                offsetPosition: [0, -69240, 0]
+            }
+        })
+    }
 }
