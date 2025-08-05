05ff81cac2feba88510f5d544b8641eccc7cf2c0
Swifter <swifter1243@gmail.com>
2025-08-05 13:26:09 -0400

    start outro notemods section

--- src/sections/outro.ts
+++ src/sections/outro.ts
@@ -1,10 +1,21 @@
 import { TIMES } from "../constants.ts";
 import { rm } from "../deps.ts";
 import { prefabs } from "../main.ts";
+import { between } from '../utilities.ts'
 
 export function outro(map: rm.V3Difficulty)
 {
     const outroScene = prefabs.outro.instantiate(map, TIMES.OUTRO)
 
+    doNotemods(map)
+
     outroScene.destroyObject(TIMES.MAP_END)
+}
+
+function doNotemods(map: rm.V3Difficulty) {
+    const OUTRO_NOTE_TRACK = 'outroNote'
+
+    map.allNotes.filter(between(575, TIMES.MAP_END)).forEach(x => {
+        x.track.add(OUTRO_NOTE_TRACK)
+    })
 }
