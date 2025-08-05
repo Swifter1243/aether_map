2877075be47723bdb0fc06249c25ccd3a2a09da7
Swifter <swifter1243@gmail.com>
2025-08-05 17:01:22 -0400

    stub section 2

--- src/sections/outro.ts
+++ src/sections/outro.ts
@@ -56,55 +56,64 @@ function doNotemods(map: rm.V3Difficulty) {
         })
     }
 
-    rm.assignPathAnimation(map, {
-        track: WHEEL_EFFECT_TRACK,
-        beat: 575 - 10,
-        animation: {
-            dissolve: [[0,wheelFromBeat(4)],[1,wheelFromBeat(2)]]
-        }
-    })
+    section1()
+    section2()
 
-    rm.assignPathAnimation(map, {
-        track: WHEEL_EFFECT_TRACK,
-        beat: 575,
-        animation: {
-            dissolve: [1]
-        }
-    })
+    function section1() {
+        rm.assignPathAnimation(map, {
+            track: WHEEL_EFFECT_TRACK,
+            beat: 575 - 10,
+            animation: {
+                dissolve: [[0,wheelFromBeat(4)],[1,wheelFromBeat(2)]]
+            }
+        })
 
-    applyBlackNotes(575)
-    map.allNotes.filter(approximately(575)).forEach(x => {
-        noteHop(x)
-    })
+        rm.assignPathAnimation(map, {
+            track: WHEEL_EFFECT_TRACK,
+            beat: 575,
+            animation: {
+                dissolve: [1]
+            }
+        })
 
-    outroRotationMovement(573, [[4,-4,0,0],[0,0,0,0.5]])
-    outroRotationMovement(573, [0,0,0], 2, 'easeOutBack')
-    
-    wheelEffect(map, 10, [575, 576, 578, 579, 581])
-    map.allNotes.filter(between(576, 581)).forEach(x => {
-        x.life = WHEEL_LIFE
-        x.track.add(WHEEL_EFFECT_TRACK)
-    })
+        applyBlackNotes(575)
+        map.allNotes.filter(approximately(575)).forEach(x => {
+            noteHop(x)
+        })
 
-    applyWhiteNotes(581)
+        outroRotationMovement(573, [[4,-4,0,0],[0,0,0,0.5]])
+        outroRotationMovement(573, [0,0,0], 2, 'easeOutBack')
+        
+        wheelEffect(map, 10, [575, 576, 578, 579, 581])
+        map.allNotes.filter(between(576, 581)).forEach(x => {
+            x.life = WHEEL_LIFE
+            x.track.add(WHEEL_EFFECT_TRACK)
+        })
 
-    outroRotationMovement(581, [[-4,-3,0,0],[0,0,0,0.5]])
-    outroRotationMovement(581, [[-4,-3,30,0],[0,0,0,0.5]], 2, 'easeOutCirc')
-    map.allNotes.filter(approximately(583)).forEach(x => {
-        x.animation.offsetWorldRotation = [[...randomVec3(3, rm.random), 0], [0,0,0,0.5,'easeOutCirc']]
-    })
+        applyWhiteNotes(581)
 
-    outroRotationMovement(585, [[-4,-3,0,0],[0,0,0,0.5]], 2, 'easeOutBack')
+        outroRotationMovement(581, [[-4,-3,0,0],[0,0,0,0.5]])
+        outroRotationMovement(581, [[-4,-3,30,0],[0,0,0,0.5]], 2, 'easeOutCirc')
+        map.allNotes.filter(approximately(583)).forEach(x => {
+            x.animation.offsetWorldRotation = [[...randomVec3(3, rm.random), 0], [0,0,0,0.5,'easeOutCirc']]
+        })
 
-    map.allNotes.filter(between(583, 589)).forEach(x => {
-        if (between(585, 587)(x)) {
-            const dist = x.beat - 583
-            noteHop(x, 5 * dist, dist)
-        }
-        else {
-            noteHop(x, 10)
-        }
+        outroRotationMovement(585, [[-4,-3,0,0],[0,0,0,0.5]], 2, 'easeOutBack')
 
-        assignDirectionalRotation(x)
-    })
+        map.allNotes.filter(between(583, 589)).forEach(x => {
+            if (between(585, 587)(x)) {
+                const dist = x.beat - 583
+                noteHop(x, 5 * dist, dist)
+            }
+            else {
+                noteHop(x, 10)
+            }
+
+            assignDirectionalRotation(x)
+        })
+    }
+
+    function section2() {
+
+    }
 }
