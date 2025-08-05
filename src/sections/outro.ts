d2ae2478d6cb92a0ff6bf6d7776705fa52927755
Swifter <swifter1243@gmail.com>
2025-08-05 14:27:18 -0400

    wheel lead-in animation

--- src/sections/outro.ts
+++ src/sections/outro.ts
@@ -1,10 +1,11 @@
 import { TIMES } from "../constants.ts";
 import { rm } from "../deps.ts";
 import { setDirectionalMagnitude } from '../effects.ts'
+import { simpleRotationPath } from '../effects.ts'
 import { assignDirectionalRotation } from '../effects.ts'
 import { noteHop, wheelEffect } from '../effects.ts'
 import { prefabs } from "../main.ts";
-import { approximately, between } from '../utilities.ts'
+import { approximately, beatsToObjectSpawnLife, between } from '../utilities.ts'
 
 export function outro(map: rm.V3Difficulty)
 {
@@ -17,8 +18,15 @@ export function outro(map: rm.V3Difficulty)
 
 function doNotemods(map: rm.V3Difficulty) {
     const OUTRO_NOTE_TRACK = 'outroNote'
+
+    const WHEEL_EFFECT_TRACK = 'outroWheelTrack'
+    const WHEEL_LIFE = 8
+    const wheelFromBeat = beatsToObjectSpawnLife(WHEEL_LIFE)
+
     applyWhiteNotes(0)
     setDirectionalMagnitude(map, 20, 575)
+
+    const outroRotationMovement = simpleRotationPath(map, OUTRO_NOTE_TRACK)
 
     map.allNotes.filter(between(575, TIMES.MAP_END)).forEach(x => {
         x.track.add(OUTRO_NOTE_TRACK)
@@ -46,12 +54,35 @@ function doNotemods(map: rm.V3Difficulty) {
         })
     }
 
+    rm.assignPathAnimation(map, {
+        track: WHEEL_EFFECT_TRACK,
+        beat: 575 - 10,
+        animation: {
+            dissolve: [[0,wheelFromBeat(4)],[1,0.5]]
+        }
+    })
+
+    rm.assignPathAnimation(map, {
+        track: WHEEL_EFFECT_TRACK,
+        beat: 575,
+        animation: {
+            dissolve: [1]
+        }
+    })
+
     applyBlackNotes(575)
     map.allNotes.filter(approximately(575)).forEach(x => {
         noteHop(x)
     })
 
+    outroRotationMovement(573, [[4,-4,0,0],[0,0,0,0.5]])
+    outroRotationMovement(573, [0,0,0], 2, 'easeOutBack')
+    
     wheelEffect(map, 10, [575, 576, 578, 579, 581])
+    map.allNotes.filter(between(576, 581)).forEach(x => {
+        x.life = WHEEL_LIFE
+        x.track.add(WHEEL_EFFECT_TRACK)
+    })
 
     applyWhiteNotes(581)
 
